d40468db29725c3c576b51d55508a6be5c429762
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-08-14 06:22:35 +0300

    Fix all linter errors

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,5 +1,7 @@
-class V1::GreetingsController < ApplicationController
-  def index
-    render json: { greeting: Greeting.all.sample.message }.to_json
+module V1
+  class GreetingsController < ApplicationController
+    def index
+      render json: { greeting: Greeting.all.sample.message }.to_json
+    end
   end
 end
